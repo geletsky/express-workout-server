2734bfa5aacca9d58473496c592dfdd0976d8b76
Geletsky Dev <nsurcov98@gmail.com>
2024-10-14 20:34:01 +0300

    Added database

---
+++ server.js
@@ -0,0 +1,31 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import morgan from 'morgan'
+
+import authRoutes from './app/auth/auth.routes.js'
+import { prisma } from './app/prisma.js'
+
+dotenv.config()
+
+const app = express()
+
+async function main() {
+	if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+
+	app.use(express.json())
+	app.use('/api/auth', authRoutes)
+
+	const PORT = 7777
+
+	app.listen(PORT, console.log(`Server is running on PORT - ${PORT}`))
+}
+
+main()
+	.then(async () => {
+		await prisma.$disconnect()
+	})
+	.catch(async e => {
+		console.error(e)
+		await prisma.$disconnect()
+		process.exit(1)
+	})
