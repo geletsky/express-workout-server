e206f36a1d806e25bd39e137aa438b85234d8ea0
Geletsky Dev <nsurcov98@gmail.com>
2024-10-15 18:15:00 +0300

    Added workout log

--- app/workout/workout.routes.js
+++ app/workout/workout.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import { protect } from '../middleware/auth.middleware.js'
 
+import { createWorkoutLog } from './log/workout-log.controller.js'
 import {
 	createWorkout,
 	deleteWorkout,
@@ -17,5 +18,6 @@ router.route('/').get(protect, getWorkouts)
 router.route('/:id').get(protect, getWorkout)
 router.route('/:id').put(protect, updateWorkout)
 router.route('/:id').delete(protect, deleteWorkout)
+router.route('/log/:id').post(protect, createWorkoutLog)
 
 export default router
